baf1cde91a83654dbdd5f7732dcb7e4b04afb7fa
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-06-17 14:36:32 +0530

    insertionsort

---
+++ insertion.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void insertionsort(int a[],int n){
+    int i,hole,value;
+    for(i=1;i<n;i++){
+   value=a[i];
+   hole=i;
+   while(hole>0 && a[hole-1]>value)
+   {
+       a[hole]=a[hole-1];
+       hole=hole-1;
+   }
+   a[hole]=value;
+    }
+     for(i=0;i<n;i++)
+     printf("%d\t",a[i]);
+}
+int main() {
+ int arr[]={5,4,2,7,3,6,1};
+ int size=sizeof(arr)/sizeof(arr[0]);
+ insertionsort(arr,size);
+ return 0;
+}
