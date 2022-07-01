49f3feaa8ce1851758643df613094a32a877a09a
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-07-01 12:22:03 +0530

    rev

---
+++ rev.c
@@ -0,0 +1,13 @@
+// rev
+#include <stdio.h>
+int main() {
+    int n=123,rem,rev=0;
+  
+    while(n>0){
+    rem=n%10;
+    rev=rev*10+(rem);
+    n=n/10;
+    }
+       printf("rev:%d",rev);
+return 0;
+}
