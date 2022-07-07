98694819828fcd04db7405c1b75b9a2be6b8d3e1
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-07-07 17:21:40 +0530

    Update CIRCULAR_SINGLY_LL.c
    begin:6 5 4 3 2 1 
    end:6 5 4 3 2 1 0 
    certain:6 5 4 3 9 2 1 0 
    delete 1st:5 4 3 9 2 1 0 
    delete 1st:5 4 3 9 2 1 
    delete certain:5 4 3 2 1

--- CIRCULAR_SINGLY_LL.c
+++ CIRCULAR_SINGLY_LL.c
@@ -37,8 +37,47 @@ while(pos!=1){
 }
 newP->next =temp->next;
 temp->next=newP;
-//return tail;
+
 }
+struct node* delete_at_first(struct node* tail){
+    
+struct node* temp;
+temp=tail->next;
+tail->next=temp->next;
+free(temp);
+temp=0;
+return tail;
+}
+
+struct node* delete_at_end(struct node* tail){
+    
+struct node* temp;
+temp=tail->next;
+while(temp->next!=tail){
+    temp=temp->next;
+}
+temp->next=tail->next;
+free(tail);
+tail=temp;
+return tail;
+}
+void delete_at_certain(struct node* tail,int pos){
+    
+struct node* temp;
+struct node* ptr;
+temp=tail->next;
+pos--;
+while(pos!=0){
+    ptr=temp;
+    temp=temp->next;
+    pos--;
+}
+ptr->next=temp->next;
+free(temp);
+temp=0;
+
+}
+
 void print(struct node* tail)
 {
 struct node* p = tail->next;
@@ -64,6 +103,15 @@ int main()
     print(tail);
     add_at_certain(tail,9,5);
     printf("\ncertain:");
+    print(tail);
+    tail=delete_at_first(tail);
+    printf("\ndelete 1st:");
+    print(tail);
+    tail=delete_at_end(tail);
+    printf("\ndelete 1st:");
+    print(tail);
+    delete_at_certain(tail,4);
+    printf("\ndelete certain:");
     print(tail);
 return 0;
 }
