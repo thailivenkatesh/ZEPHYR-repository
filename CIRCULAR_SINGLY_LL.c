a8001ef4ac0d785afb0b7f6d617e9e4e5ec4b900
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-07-07 14:43:38 +0530

    CIRCULAR_SINGLY_LL.c

---
+++ CIRCULAR_SINGLY_LL.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node {
+int data;
+struct node* next;
+};
+struct node* circularSingly(int data)
+{
+struct node* temp = malloc(sizeof(struct node));
+temp->data = data;
+temp->next = temp;
+return temp;
+}
+int main()
+{
+int data = 34;
+struct node* tail;
+tail = circularSingly(data);
+printf("%d\n", tail->data);
+return 0;
+}
