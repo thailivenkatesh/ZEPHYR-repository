1fab35c72332b4b0d72e9f7b91edc44496993a1c
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-07-08 17:17:33 +0530

    Update CIRCULAR_DOUBLY_LL.c
    begin:30 31 32 34 35 
    
    end:30 31 32 34 35 36 37

--- CIRCULAR_DOUBLY_LL.c
+++ CIRCULAR_DOUBLY_LL.c
@@ -20,6 +20,22 @@ tail->prev = newP;
 tail->next = newP;
 
 }
+
+struct node* addAtend(struct node* tail, int data){
+    
+struct node* newP =  malloc(sizeof(struct node));
+newP->prev = newP;
+newP->data = data;
+newP->next = newP;
+
+newP->next = tail->next;
+newP->prev = tail;
+tail->next = newP;
+tail = newP;
+return tail;
+}
+
+
 void print(struct node* tail){
 
 struct node* temp = tail->next;
@@ -38,7 +54,11 @@ int main()
     tail->data=35;
     tail->prev=tail;
     tail->next=tail;
+	printf("\nbegin:");
     tail = addAtBeg(tail, 34);tail = addAtBeg(tail, 32);tail = addAtBeg(tail, 31);tail = addAtBeg(tail, 30);
     print(tail);
+	printf("\nend:");
+	tail = addAtend(tail, 36);tail = addAtend(tail, 37);
+	print(tail);
 return 0;
 }
