3989b59b3e8ad9f688f7bf48c5efed90dd4949c3
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-07-09 11:17:52 +0530

    Update CIRCULAR_DOUBLY_LL.c
    begin:30 31 32 34 35 
    
    end:30 31 32 34 35 36 37 
    
    certain:30 31 32 67 35 36 37

--- CIRCULAR_DOUBLY_LL.c
+++ CIRCULAR_DOUBLY_LL.c
@@ -2,63 +2,87 @@
 #include <stdlib.h>
 struct node
 {
-struct node* prev;
-int data;
-struct node *next;
+	struct node* prev;
+	int data;
+	struct node *next;
 };
 
 struct node* addAtBeg(struct node* tail, int data){
     
-struct node* newP =  malloc(sizeof(struct node));
-newP->prev = newP;
-newP->data = data;
-newP->next = newP;
+	struct node* newP =  malloc(sizeof(struct node));
+	newP->prev = newP;
+	newP->data = data;
+	newP->next = newP;
 
-newP->prev = tail->next;
-newP->next = tail->next;
-tail->prev = newP;
-tail->next = newP;
+	newP->prev = tail->next;
+	newP->next = tail->next;
+	tail->prev = newP;
+	tail->next = newP;
 
 }
 
 struct node* addAtend(struct node* tail, int data){
     
-struct node* newP =  malloc(sizeof(struct node));
-newP->prev = newP;
-newP->data = data;
-newP->next = newP;
+	struct node* newP =  malloc(sizeof(struct node));
+	newP->prev = newP;
+	newP->data = data;
+	newP->next = newP;
 
-newP->next = tail->next;
-newP->prev = tail;
-tail->next = newP;
-tail = newP;
-return tail;
+	newP->next = tail->next;
+	newP->prev = tail;
+	tail->next = newP;
+	tail = newP;
+	return tail;
+}
+void addAtcertain(struct node* tail, int data,int pos){
+    
+	struct node* newP =  malloc(sizeof(struct node));
+	newP->prev = newP;
+	newP->data = data;
+	newP->next = newP;
+	pos--;
+	struct node* temp;
+	struct node* ptr=tail->next;
+	while(pos!=0){
+		temp=ptr;
+		ptr=ptr->next;
+		pos--;
+    
+	}
+	newP->next = ptr->prev;
+	newP->prev = temp->next;
+	temp->next = newP;
+	ptr->prev = newP;
+
 }
 
 
 void print(struct node* tail){
 
-struct node* temp = tail->next;
-do
-{
-printf("%d ", temp->data);
-temp = temp->next;
-} while(temp != tail->next);
+	struct node* temp = tail->next;
+	do
+	{
+	printf("%d ", temp->data);
+	temp = temp->next;
+	} while(temp != tail->next);
 
-printf("\n");
+	printf("\n");
 }
 int main()
 {
-    struct node* tail = NULL;
-    tail=malloc(sizeof(struct node));
-    tail->data=35;
-    tail->prev=tail;
-    tail->next=tail;
+	struct node* tail = NULL;
+	tail=malloc(sizeof(struct node));
+	tail->data=35;
+	tail->prev=tail;
+	tail->next=tail;
 	printf("\nbegin:");
-    tail = addAtBeg(tail, 34);tail = addAtBeg(tail, 32);tail = addAtBeg(tail, 31);tail = addAtBeg(tail, 30);
-    print(tail);
+	tail = addAtBeg(tail, 34);tail = addAtBeg(tail, 32);tail = addAtBeg(tail, 31);tail = addAtBeg(tail, 30);
+	print(tail);
 	printf("\nend:");
 	tail = addAtend(tail, 36);tail = addAtend(tail, 37);
 	print(tail);
-return 0;
+	printf("\ncertain:");
+	addAtcertain(tail, 67,4);
+	print(tail);
+	return 0;
 }
