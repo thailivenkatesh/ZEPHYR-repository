4cf7b905749aa177af2db42570d74b8bfb2c3e86
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-07-25 12:52:26 +0530

    Create  concatenate.c

---
+++ concatenate.c
@@ -0,0 +1,20 @@
+// strcat in C Programming to concatenate strings  
+#include <stdio.h> 
+#include<string.h>
+int main()
+{
+   char str1[] = "Learn";
+   char str2[] = " C Lang";
+
+/*	printf("lib fun\n");
+   strcat(str1, str2);		
+   puts(str1);*/
+   int i,j=0;
+   	printf("\nown fun\n");
+   j=strlen(str1);
+   	for(i=0;str2[i]!='\0';i++){
+   	    str1[j++]=str2[i];
+   	}
+   	str1[j]='\0';
+  puts(str1);
+}
