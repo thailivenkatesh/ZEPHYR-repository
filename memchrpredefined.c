33e6f18961691fb6eed89bceca9533394fd16d7d
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-07-25 12:39:08 +0530

    Update memchrpredefined.c

--- memchrpredefined.c
+++ memchrpredefined.c
@@ -1,24 +1,23 @@
 //The C memchr function is a String Function, which will find the first occurrence of the character, and returns the pointer to it.
-#include  <stdio.h>
+##include <stdio.h> 
+#include<string.h>
 
-int main(int argc, const char * argv[])
-{
-    char name[50],ch;
-    int i = 0,j=0;
-    
- 	printf("\n Please Enter any String  : ");
-	gets(name);	
-	printf("\n Please Enter any Charcater that you want to search for  : ");
-	scanf("%c", &ch);
-    while (name[i] != '\0')
-    {
-        
-       if(ch==name[i]){
-        printf("%s",name+i);
-        i++;j++;
-        break;
-    }i++;
-    }
-    if(j==0)printf("not found");
-    return 0;
-}
+int main()
+{  
+   	char str[] = "C Programming Language";
+   	char ch = 'L';
+   	char *result;
+   	char *result2;
+   	printf("lib fun\n");
+   	result = memchr(str, ch, strlen(str));
+   	printf("%s",result);
+   	int i;
+   	printf("\nown fun\n");
+   	for(i=0;str[i]!='\0';i++){
+   	    if(str[i]==ch){
+   	    printf("%s",str+i);
+   	    break;
+   	    }
+   	}
+   	return 0;
+} 
