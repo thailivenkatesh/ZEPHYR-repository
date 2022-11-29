d8abacb8f6737e2e69c0c183578d225f21f7fe96
thailivenkatesh <59580220+thailivenkatesh@users.noreply.github.com>
2022-11-29 15:26:20 +0530

    Update bitreversal.c

--- bitreversal.c
+++ bitreversal.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 //bit reversal function
+/******************16bit*********************/
 unsigned int ReverseTheBits(unsigned int n)
 {
     unsigned int i= 0;
@@ -15,5 +16,25 @@ unsigned int ReverseTheBits(unsigned int n)
 }
 int main(){
    printf("%d", ReverseTheBits(01));
+   return 0;
+}
+/******************8bit*********************/
+#include<stdio.h>
+//bit reversal function
+unsigned int ReverseTheBits(unsigned int n)
+{
+     int i;
+     int rev = 0;         //  Assign num to the tmp
+    for(i= 0; i < 8; ++i)
+    {
+        if((n & (1 << i))) // check set bits of num
+        {
+            rev |= 1 << ((8 - 1) - i); //putting the set bits of num in tmp
+        }
+    }
+    return rev;
+}
+int main(){
+   printf("%d", ReverseTheBits(15));
    return 0;
 }
